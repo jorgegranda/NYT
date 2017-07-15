1735b1e4dd23368e3ac10396b6c5d56df80a0520
burningserenity <burningserenity@bitmai.la>
2017-07-15 11:50:49 -0400

    Reverted to original New York Times.html and NYTlogic.js files. Added multiple displays; need to add multiple boxes containing numbers.

--- NYTlogic.js
+++ NYTlogic.js
@@ -6,6 +6,7 @@
     author: "",
     link: ""
   }
+  var i = 0;
 
   function queryNYT()
   {
@@ -31,6 +32,40 @@
       $("#snippet").text(renderQuery.snippet);
       $("#date").text(renderQuery.date);
       $("#link").text(renderQuery.link);
+
+      if (response.response.docs.length > 1)
+      {
+        for (i = 0; i < response.response.docs.length; i++)
+        {
+          var cloneDiv = $("<div>").attr('id', "#well" + i);
+          var cloneAuthor = $("<h4>").attr('id', "#author" + i);
+          var cloneSection = $("<h4>").attr('id', "#snippet" + i);
+          var cloneDate = $("<h4>").attr('id', "#date" + i);
+          var cloneLink = $("<a>").attr('id', "#link" + i);
+          var cloneHeadline = $('<h3>').attr('id', "#headline" + i);
+          var p = i + 1;
+
+          renderQuery.headline = response.response.docs[i].headline.main;
+          console.log(renderQuery.headline);
+          renderQuery.snippet = response.response.docs[i].snippet;
+          console.log(renderQuery.snippet);
+          renderQuery.date = response.response.docs[i].pub_date;
+          console.log(renderQuery.date);
+          renderQuery.author = response.response.docs[i].byline.original;
+          console.log(renderQuery.author);
+          renderQuery.link = response.response.docs[i].web_url;
+          console.log(renderQuery.link);
+
+           $('#grid-search').append('<p align = "center" class="square">'+p+'</p>');
+           cloneDiv.prependTo(".panel-body");
+           cloneAuthor.text(renderQuery.author).appendTo(cloneDiv);
+           cloneHeadline.text(renderQuery.headline).appendTo(cloneDiv);
+           cloneDate.text(renderQuery.date).appendTo(cloneDiv);
+           cloneSection.text(renderQuery.snippet).appendTo(cloneDiv);
+           cloneLink.text(renderQuery.link).appendTo(cloneDiv);
+      }
+    }
+
     }).fail(function(err) {
       throw err;
     });
